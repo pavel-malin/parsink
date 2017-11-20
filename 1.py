20384f8385607afc1e020d402d5ff105534d50de
root <root@localhost.localdomain>
2017-11-20 13:56:00 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class BookSpider(scrapy.Spider):
+    name = 'books'
+    start_urls = ['http://toscrape.com/']
+
+
+    def parse(self, response):
+        for href in response.css('.product_pod a::attr(href)').extract():
+            url = response.urljoin(href)
+            print(url)
